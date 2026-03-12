f7dd66a08283d9540217e20cd98835c06eac03e6
abdulkarim41 <abdulkarim.jatri@gmail.com>
2026-03-12 22:20:24 +0600

    feat(presentation): implement home fragment and login navigation
    - Create `HomeFragment` and its corresponding layout `fragment_home.xml`.
    - Update `fragment_login.xml` by replacing the placeholder text with a login button.
    - Set up an `OnClickListener` on the login button in `LoginFragment` to trigger navigation.
    - Configure the navigation graph to include `homeFragment` and define an action to navigate from login to home, clearing the backstack.

--- app/src/main/kotlin/com/abdulkarim/android_testing_playground/presentation/HomeFragment.kt
+++ app/src/main/kotlin/com/abdulkarim/android_testing_playground/presentation/HomeFragment.kt
@@ -3,25 +3,20 @@ package com.abdulkarim.android_testing_playground.presentation
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.abdulkarim.android_testing_playground.R
-import com.abdulkarim.android_testing_playground.databinding.FragmentLoginBinding
+import com.abdulkarim.android_testing_playground.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        _binding = FragmentLoginBinding.bind(view)
-
-        binding.loginBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_navigate_to_homeFragment)
-        }
+        _binding = FragmentHomeBinding.bind(view)
 
     }
 
